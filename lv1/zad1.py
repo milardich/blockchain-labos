f32c3242e21e638a64af1f65f59011a861204e95
Stjepan Milardic <milardic.ferit@hehe>
2023-03-21 12:47:32 +0100

    qwer

--- lv1/zad1.py
+++ lv1/zad1.py
@@ -4,8 +4,8 @@ input = 'FERIT'
 
 b58 = base58.b58encode(input)
 print(
-    f'Encoding -> input: {input} - output: {b58.decode("utf-8")}')
+    f'To base58 -> input: {input} - output: {b58.decode("utf-8")}')
 
 decoded = base58.b58decode(b58)
 print(
-    f'Decoding -> base58: {b58.decode("utf-8")}  - output: {decoded.decode("utf-8")}')
+    f'From base58 -> base58: {b58.decode("utf-8")}  - output: {decoded.decode("utf-8")}')
